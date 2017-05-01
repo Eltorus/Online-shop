6feb5ec80bba92210e8b0fe288ff7e00f4ee8574
Eltorus <zhuk.pav96@gmail.com>
2017-05-01 17:16:24 +0200

    Deleted cartfilter

--- E-commerceShop/src/by/epam/shop/util/RoundDouble.java
+++
@@ -1,9 +0,0 @@
-package by.epam.shop.util;
-
-public class RoundDouble {
-	private RoundDouble() {}
-	
-	public static double getRoundedDouble(double d) {
-		return Math.round(d * 100.0) / 100.0;
-	}
-}
